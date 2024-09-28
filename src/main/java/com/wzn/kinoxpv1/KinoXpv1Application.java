84617716f6fcaa03a1d477ff8dde50b89c1a1198
wzNaji <ludi2300@gmail.com>
2024-09-28 21:44:15 +0200

    initial

---
+++ src/main/java/com/wzn/kinoxpv1/KinoXpv1Application.java
@@ -0,0 +1,13 @@
+package com.wzn.kinoxpv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoXpv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoXpv1Application.class, args);
+    }
+
+}
